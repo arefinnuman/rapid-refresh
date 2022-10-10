a773b596449a1ed9b2ef06eee5bd5b9a2f14d1d4
arefinnuman <arefinnuman2000@gmail.com>
2022-10-10 21:22:39 +0600

    facing Some Issue

---
+++ src/Utilities/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter(
+    {
+        path: "/",
+        
+    }
+)
+
+const Routes = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Routes;
