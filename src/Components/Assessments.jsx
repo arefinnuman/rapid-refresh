bb6e50d65069642ec8c5bca75986da652f84ca7c
arefinnuman <arefinnuman2000@gmail.com>
2022-10-12 16:11:18 +0600

    Blog added

--- src/Components/Assessments.jsx
+++ src/Components/Assessments.jsx
@@ -6,32 +6,28 @@ import Topic from "./Topic";
 const Assessments = () => {
   const topics = useLoaderData();
   return (
-    <div className="min-h-screen">
-      <div className="card lg:card-side bg-base-100 shadow-l m-10 ">
-        <figure>
-          <img className="" src={image} alt="Album" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">What is the Rapid Refresh quiz maker?</h2>
-          <p>
+    <div>
+      <div className="md:m-20 flex flex-col items-center justify-center">
+        <div className="md:mx-20 m-5">
+          <h1 className="text-2xl font-bold">
+            What is the Rapid Refresh quiz maker?
+          </h1>
+          <p className="semi-bold">
             Rapid Refresh is a quiz that’s delivered at regular intervals to
             learners to help check their understanding and reinforce learning.
             Questions are set up using our easy-to-complete spreadsheet, and
             you’ll have access to timely analytics to help understand individual
             learners as well as overall performance once a quiz is complete.
           </p>
-          <h2 className="card-title">Automated delivery</h2>
-          <p>
-            Watch the magic happen as we deploy your quiz at intervals
-            determined by you. Your quiz is delivered using EdApp’s familiar
-            microlearning templates. You’ll also get to take advantage of the
-            gamification built as standard into EdApp, including leaderboards
-            and notifications.
-          </p>
         </div>
+        <img src={image} alt="" />
       </div>
-
-      <div className="md:flex sm:flex-row min-h-screen justify-center items-center">
+      <div>
+        <h1 className="text-4xl font-extrabold text-center text-primary">
+          Test YourSelf
+        </h1>
+      </div>
+      <div className="md:flex sm:flex-row justify-center items-center md:m-20">
         {topics.data.map((topic) => (
           <Topic topic={topic} key={topic.id}></Topic>
         ))}
--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -3,7 +3,67 @@ import React from "react";
 const Blog = () => {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <h1 className="text-5xl">Welcome to Blog Template</h1>
+      <article className="max-w-2xl px-6 py-24 space-y-12 bg-primary dark:text-gray-50 m-5 rounded-lg">
+        <div className="w-full mx-auto space-y-4 text-center">
+          <h1 className="text-4xl font-bold leading-tight md:text-5xl">
+            What is the purpose of react router?
+          </h1>
+        </div>
+        <div className="pt-12 border-t dark:border-gray-700">
+          <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
+            <div className="flex flex-col">
+              <p className="dark:text-gray-400">
+                ReactJS Router is mainly used for developing Single Page Web
+                Applications. React Router is used to define multiple routes in
+                the application. When a user types a specific URL into the
+                browser, and if this URL path matches any 'route' inside the
+                router file, the user will be redirected to that particular
+                route.
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+      <article className="max-w-2xl px-6 py-24 m-5 space-y-12 bg-primary dark:text-gray-50 rounded-lg">
+        <div className="w-full mx-auto space-y-4 text-center">
+          <h1 className="text-4xl font-bold leading-tight md:text-5xl">
+            How does context api works ?
+          </h1>
+        </div>
+        <div className="pt-12 border-t dark:border-gray-700">
+          <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
+            <div className="flex flex-col">
+              <p className="dark:text-gray-400">
+                The React Context API is a way for a React app to effectively
+                produce global variables that can be passed around. This is the
+                alternative to "prop drilling" or moving props from grandparent
+                to child to parent, and so on. Context is also touted as an
+                easier, lighter approach to state management using Redux.
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+      <article className="max-w-2xl px-6 py-24 m-5 space-y-12 bg-primary dark:text-gray-50 rounded-lg">
+        <div className="w-full mx-auto space-y-4 text-center">
+          <h1 className="text-4xl font-bold leading-tight md:text-5xl">
+            What is href in react ?
+          </h1>
+        </div>
+        <div className="pt-12 border-t dark:border-gray-700">
+          <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
+            <div className="flex flex-col">
+              <p className="dark:text-gray-400">
+                This href attribute contains the URL or path to the destination
+                page. It may be a relative URL or an absolute URL. In React,
+                relative URLs should always be handled by the link tag provided
+                by the React Router, and pure anchor tags should only be used
+                for absolute paths
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
     </div>
   );
 };
